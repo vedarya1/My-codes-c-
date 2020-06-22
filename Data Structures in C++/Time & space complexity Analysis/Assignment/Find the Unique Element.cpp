3d595960acfc8277fd0d6e545a5909b8f6c8ec9d
Ved Prakash <55890729+vedarya1@users.noreply.github.com>
2020-06-22 23:43:19 +0530

    added code

---
+++ Data Structures in C++/Time & space complexity Analysis/Assignment/Find the Unique Element.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int FindUnique(int input[],int size){
+	int output;
+	for(int i=0;i<size;i++)
+	{
+		output^=input[i];
+	}
+	return output;
+}
+
+
+
+
+int main(){
+	int size;
+	cin>>size;
+	
+	int *input = new int[1+size];
+	
+	for(int i = 0;i<size;i++)
+		cin>>input[i];
+		cout<<FindUnique(input,size)<<endl;	
+	
+}
