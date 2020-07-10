7210b8c553d047dacbd5abd511aa6a5341301f56
Ved Prakash <55890729+vedarya1@users.noreply.github.com>
2020-07-10 14:42:11 +0530

    Add files via upload

---
+++ Data Structures in C++/Linked list-1/Take input/Length-of-LL.cpp
@@ -0,0 +1,54 @@
+#include<iostream>
+using namespace std;
+
+class Node{
+	public:
+		int data;
+		Node *next;
+		
+		Node(int data){  //one argument constructor
+			this->data= data;
+			this->next = NULL;
+		}
+};
+
+Node *takeInput(){
+	int data;
+	cin>>data;
+	Node *head = NULL;
+	Node *tail = NULL;
+	while(data != -1){
+		Node *newNode = new Node(data);
+		if(head == NULL){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail->next = newNode;
+			tail = newNode;
+		}
+		cin>>data;
+	}
+	return head;
+}
+
+int length(Node *head){
+	int count = 0;
+	while(head != NULL){
+		count++;
+		head = head->next;
+	}
+	return count;
+}
+
+
+
+int main(){
+     int t;
+     cin>>t;
+     while(t--){
+     	Node *head = takeInput();
+     	cout<<length(head)<<endl;
+	 }
+	 return 0;
+}
