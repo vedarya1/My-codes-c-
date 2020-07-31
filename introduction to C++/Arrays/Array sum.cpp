f97f1ef1af2306aa99f7469a7c6cf6abfd8008c1
Ved Prakash <55890729+vedarya1@users.noreply.github.com>
2020-07-31 14:51:58 +0530

    Update Array sum.cpp

--- introduction to C++/Arrays/Array sum.cpp
+++ introduction to C++/Arrays/Array sum.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main(){
 int n;
-cin>>n;
+ 
+cin >> n;
  int *arr = new int[1000000];
  int sum = 0;
- for(int i = 0;i<n;i++){
- 	cin>>arr[i];
- 	sum+=arr[i];
+ for(int i = 0; i < n; i++){
+ 	cin >> arr[i];
+ 	sum += arr[i];
  }
-    cout<<sum<<endl;
+    cout << sum << endl;
 }
