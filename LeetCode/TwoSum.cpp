350c5420b16755536e189dada2199f88d042a723
Ved Prakash <55890729+vedarya1@users.noreply.github.com>
2020-10-03 17:01:43 +0530

    Update TwoSum.cpp

--- LeetCode/TwoSum.cpp
+++ LeetCode/TwoSum.cpp
@@ -4,6 +4,7 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
          
         vector<int> res;
+        
         for (int i=0;i<nums.size()-1;i++){
             for (int j=i+1;j<nums.size();j++){
                 if (nums[i]+nums[j]==target){
